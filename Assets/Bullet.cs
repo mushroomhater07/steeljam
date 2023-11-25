c44f2dbd1a1471d81fd75a6018e9bdab70b18719
Frederik T <frederik.trommer@t-online.de>
2023-11-25 16:19:02 +0000

    Fred

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -25,7 +25,11 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer ==3)
-                { Destroy(gameObject); }
+        if(collision.gameObject.layer ==3) { 
+            Destroy(gameObject); 
+        }
+        if (collision.gameObject.layer == 6) { 
+            moveSpeed = moveSpeed*-1; 
+        }
     }
 }
