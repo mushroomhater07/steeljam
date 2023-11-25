6ee08381c879fbc6f6deab0df3e08e2f22020416
Shalev <103824094+mushroomhater07@users.noreply.github.com>
2023-11-25 14:53:06 -0800

    Update LogicController.cs

--- Assets/script/Controller/LogicController.cs
+++ Assets/script/Controller/LogicController.cs
@@ -1,38 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogicController : MonoBehaviour
 {
-    public float score = 0;
+    private float score = 0;
+    private float currenthealth = 0;
+    [SerializeField] float maxHealth;
+    
+    [SerializeField] private bool nether;
+    [SerializeField] private float DMGinNetherRate;
+    
     public TextMeshProUGUI scoreText;
     public GameObject GameOverScreen;
-    public float spawnNumber = 0;
 
-    public void AddScore()
-    { 
-        score++;
-        scoreText.text = score.ToString();
-    }
+    private Slidersss slidersss_instance;
 
-    public void RestartGame()
+    public void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        slidersss_instance = GetComponent<Slidersss>();
+        slidersss_instance.maxHealth = this.maxHealth;
     }
 
-    public void GameOver()
-    { 
-        GameOverScreen.SetActive(true); 
+    private void Update()
+    {
+        if (nether) currenthealth -= Time.deltaTime * DMGinNetherRate;
+        slidersss_instance.currentHealth = currenthealth;
+        UnityEngine.Tilemaps.Tilemap.
+            
     }
 
-    public void GetStrawberry()
-    {
-        spawnNumber--;
-    }
-    public void AddStrawberry()
-    {
-        spawnNumber++;
-    }
+    public void AddScore() { score++; scoreText.text = score.ToString();}  
+    public void RestartGame() { SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
+    public void GameOver() { GameOverScreen.SetActive(true); }
 }
