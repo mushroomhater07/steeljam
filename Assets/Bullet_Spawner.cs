36fa90d96abaa49ea64212943e95181597a37772
Frederik T <frederik.trommer@t-online.de>
2023-11-25 16:44:25 +0000

    Fred

--- Assets/Bullet_Spawner.cs
+++ Assets/Bullet_Spawner.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class Buller_Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
-    [SerializeField] private float spawnRate = 3;
+    //[SerializeField] private float spawnRate = 3;
+    [SerializeField] private float EnemyNumber = 1;
+    private float spawnNumber = 0;
     private float spawnDelay = 2;
     private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        timer -= spawnDelay;   
+        spawnNumber = 0;
+        timer -= spawnDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer < spawnRate)
+        /*if(timer < spawnRate)
         {
             timer += Time.deltaTime;
         }
         else
         {
             timer = 0;
+        */
+        if (spawnNumber < EnemyNumber)
+        {
             Instantiate(bullet, transform.position, transform.rotation);
+            spawnNumber++;
         }
     }
 }
