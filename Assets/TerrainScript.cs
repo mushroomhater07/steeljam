f7be6ac4c0a08bc86825ff7353f8851182e48cf2
Frederik T <frederik.trommer@t-online.de>
2023-11-26 03:35:55 +0000

    Fred

--- Assets/TerrainScript.cs
+++ Assets/TerrainScript.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
 public class TerrainScript : MonoBehaviour
 {
+    private LogicController LogicController_instance;
     [SerializeField] public Tilemap Tiles;
     private Color color1 = new Vector4(1, 1, 1, 1);
     private Color color2 = new Vector4(100 / 255f, 55 / 255f, 115 / 255f, 255 / 255f);
@@ -13,6 +15,7 @@ public class TerrainScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        LogicController_instance = FindObjectOfType<LogicController>();
         Tiles = GetComponent<Tilemap>();
         Tiles.color = color2;
     }
@@ -23,4 +26,18 @@ public class TerrainScript : MonoBehaviour
 
     }
 
+    public void ChangeColor()
+    {
+        if (LogicController_instance.nether)
+        {
+            Tiles.color = color2;
+        }
+        else
+        {
+            Tiles.color = color1;
+        }
+
+    }
+
 }
+
